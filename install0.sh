e813302da8ff207c23c4c1b2851e9dbc09cf6b0e
Bertrand Juglas <b@juglas.name>
2017-11-30 15:26:02 +0100

    use branch master instead of dev for git repo

--- install0.sh
+++ install0.sh
@@ -6,6 +6,7 @@ sudo yum install maven libappindicator -y
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.rpm"
 sudo yum localinstall jdk-8u152-linux-x64.rpm -y
 git clone https://github.com/iotaledger/iri
+git checkout master
 sudo mv iri /var/lib
 sudo mv installIRI.sh /var/lib/iri
 sudo useradd -s /sbin/nologin -M -d /var/lib/iri iri
