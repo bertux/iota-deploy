ada3065beac0fee91199707fc6eeba54eb17a69f
Bertrand Juglas <b@juglas.name>
2017-11-30 22:48:22 +0100

    copy file instead of move to not modify repo

--- installPeerManager.sh
+++ installPeerManager.sh
@@ -3,7 +3,7 @@ curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum -y install nodejs
 sudo yum -y install gcc-c++ make
 sudo npm install -g iota-pm
-sudo mv iota-pm.service /usr/lib/systemd/system/
+sudo cp iota-pm.service /usr/lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable iota-pm
 sudo systemctl start iota-pm
