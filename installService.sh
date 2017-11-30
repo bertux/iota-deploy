fda4f254378e48cf548d7eda655293d246cda01f
Bertrand Juglas <b@juglas.name>
2017-11-30 22:49:05 +0100

    copy files instead of move to not modify repo

--- installService.sh
+++ installService.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo mv iri.sysconfig /etc/sysconfig/iri
-sudo mv iri.service /usr/lib/systemd/system/
+sudo cp iri.sysconfig /etc/sysconfig/iri
+sudo cp iri.service /usr/lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable iri.service
 sudo systemctl start iri.service
