400b2e6a6aff4e894aff3ba416a9e197779b7e2e
Bertrand Juglas <b@juglas.name>
2017-11-30 15:03:07 +0100

    git installed first by main script

--- install0.sh
+++ install0.sh
@@ -2,7 +2,7 @@
 # update first before install
 # sudo yum update -y
 sudo yum install epel-release -y
-sudo yum install maven git libappindicator -y
+sudo yum install maven libappindicator -y
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.rpm"
 sudo yum localinstall jdk-8u152-linux-x64.rpm -y
 git clone https://github.com/iotaledger/iri
